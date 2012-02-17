5a1ddf33f54ee79b6b613832bbc3675cb8fdd2df
Federico Spini <federico.spini@gmail.com>
2012-02-17 17:58:22 +0100

    fixed pointset-filter

--- test/test.pointset.js
+++ test/test.pointset.js
@@ -61,6 +61,7 @@
     });
 
     describe('#merge(precision)', function () {
+
       it('does not change a pointset with nothig to merge', function () {
         var points = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.26,0],[0,0,.19],[.2,.2,.21],[.2,.2,.23]]
           , expectedPoints = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.26,0],[0,0,.19],[.2,.2,.21],[.2,.2,.23]]
@@ -77,7 +78,7 @@
         expect(pointset.equals(expectedPointset)).to.be.ok();
       });
 
-      it('filter simple duplicate', function () {
+      it('filters simple duplicate', function () {
         var points = [[.2,0,0],[0,.2,0],[.2,0,0],[.2,0,0],[.2,0,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.26,0],[0,0,.19],[.2,.2,.21],[.2,.2,.23],[.21,0,0]]
           , expectedPoints = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.26,0],[0,0,.19],[.2,.2,.21],[.2,.2,.23]]
           , expectedPointset = new simplexn.PointSet(expectedPoints)
@@ -93,7 +94,7 @@
         expect(pointset.equals(expectedPointset)).to.be.ok();
       });
 
-      it('fix precision for all point in a pointset', function () {
+      it('fixes precision for all point in a pointset', function () {
         var points = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.24,0],[0,0,.19],[.2222,.2,.21],[.2,.2,.23]]
           , expectedPoints = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.24,0],[0,0,.19],[.22,.2,.21],[.2,.2,.23]]
           , expectedPointset = new simplexn.PointSet(expectedPoints)
@@ -109,7 +110,7 @@
         expect(pointset.equals(expectedPointset)).to.be.ok();
       });
 
-      it('fix precision and merge duplicates for all point in a pointset', function () {
+      it('fixes precision and merge duplicates for all point in a pointset', function () {
         var points = [[.2,0,0],[0,.2,0],[.2,0,0],[.2,0,0],[.2,0,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.24,0],[0,0,.19],[.2222,.2,.21],[.2,.2,.23],[.21,0,0]]
           , expectedPoints = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2]]
           , expectedPointset = new simplexn.PointSet(expectedPoints)
@@ -124,23 +125,29 @@
         expect(areEqual(indices, expectedIndices)).to.be.ok();
         expect(pointset.equals(expectedPointset)).to.be.ok();
       });
-    })
-  });
+    });
 
-  describe('#filter(iterator)', function () {
-    it('correctly filter a pointset', function () {
-      var points = [[1.01,1.01,5.0,0],[1.01,2.01,-5.0,0],[1.01,-1.01,2.0,0],[-1.01,1.01,0.01,-0.60],[1.01,0,0.0,0],[6.01,1.01,-1.0,0]]
-        , expectedPoints = [[1.01,1.01,5.0,0],[1.01,-1.01,2.0,0],[-1.01,1.01,0.01,-0.60],[1.01,0,0.0,0]]
-        , expectedPointset = new simplexn.PointSet(expectedPoints)
-        , pointset = new simplexn.PointSet(points, 3)
-        , clonedPointset = new simplexn.PointSet(points, 3)
-        , fileteredPointeset = pointset.filter(function (point) {
-            return point[2] >= 0;
-          })
-        ;
+    describe('#filter(iterator)', function () {
 
-      expect(filteredPointset.equals(expectedPointset)).to.be.ok();
-      expect(pointset.equals(clonedPointset)).to.be.ok();
+      it('correctly filters a pointset', function () {
+        var points = [[1.01,1.01,5.0,0],[1.01,2.01,-5.0,0],[1.01,-1.01,2.0,0],[-1.01,1.01,0.01,-0.60],[1.01,0,0.0,0],[6.01,1.01,-1.0,0]]
+          , expectedPoints = [[1.01,1.01,5.0,0],[1.01,-1.01,2.0,0],[-1.01,1.01,0.01,-0.60],[1.01,0,0.0,0]]
+          , expectedPointset = new simplexn.PointSet(expectedPoints)
+          , pointset = new simplexn.PointSet(points, 4)
+          , clonedPointset = new simplexn.PointSet(points, 4)
+          , filteredPointset
+          ;
+
+        filteredPointset = pointset.filter(function (point) {
+          return point[2] >= 0;
+        });
+
+        console.log(expectedPointset, filteredPointset);
+
+        expect(filteredPointset.equals(expectedPointset)).to.be.ok();
+        expect(pointset.equals(clonedPointset)).to.be.ok();
+      });
+
     });
   });
 
