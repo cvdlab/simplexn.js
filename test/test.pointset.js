f2d45ab6cc0b9d0769cc606417179a5fa6dd81f7
Enrico Marino <enrico.marino@email.com>
2012-02-17 17:23:50 +0100

    added method and test pointset equals

--- test/test.pointset.js
+++ test/test.pointset.js
@@ -18,6 +18,48 @@
 
   describe('PointSet', function () {
     
+    describe('#equals(pointSet)', function () {
+      
+      it('is equal to a point set with the same points', function () {
+        var points = [[1,2,3],[4.1,4.2,4.3],[0.0,Math.PI,-9]]
+          , pointSet1 = new simplexn.PointSet(points)
+          , pointSet2 = new simplexn.PointSet(points)
+          ;
+
+        expect(pointSet1.equals(pointSet2)).ok();
+      });
+
+      it('is not equal to a point set with a permutation of the same points', function () {
+        var points1 = [[1,2,3],[4.1,4.2,4.3],[0.0,Math.PI,-9]]
+          , points2 = [[4.1,4.2,4.3],[0.0,Math.PI,-9],[1,2,3]]
+          , pointSet1 = new simplexn.PointSet(points1)
+          , pointSet2 = new simplexn.PointSet(points2)
+          ;
+
+        expect(pointSet1.equals(pointSet2)).not.ok();
+      });
+
+      it('is not equal to a point set with a different dim', function () {
+        var points1 = [[1,2,3],[4.1,4.2,4.3],[0.0,Math.PI,-9]]
+          , points2 = [[1,2],[4.1,4.2],[0.0,Math.PI]]
+          , pointSet1 = new simplexn.PointSet(points1)
+          , pointSet2 = new simplexn.PointSet(points2)
+          ;
+
+        expect(pointSet1.equals(pointSet2)).not.ok();
+      });
+
+      it('is not equal to a point set with a different length', function () {
+        var points1 = [[1,2,3],[4.1,4.2,4.3],[0.0,Math.PI,-9]]
+          , points2 = [[1,2,3],[4.1,4.2,4.3],[0.0,Math.PI,-9],[0,1,2]]
+          , pointSet1 = new simplexn.PointSet(points1)
+          , pointSet2 = new simplexn.PointSet(points2)
+          ;
+
+        expect(pointSet1.equals(pointSet2)).not.ok();
+      });
+    });
+
     describe('#merge(precision)', function () {
       it('does not change a pointset with nothig to merge', function () {
         var points = [[.2,0,0],[0,.2,0],[0,0,.2],[.2,.2,.2],[.21,0,0],[0,.26,0],[0,0,.19],[.2,.2,.21],[.2,.2,.23]]
