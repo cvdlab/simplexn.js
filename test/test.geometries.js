0f0f96c9b610ca19eff43c4becb9d53c58d5a9c1
Federico Spini <federico.spini@gmail.com>
2012-02-25 15:31:58 +0100

    fixed simplicialcomplex-extrude

--- test/test.geometries.js
+++ test/test.geometries.js
@@ -7,7 +7,7 @@
       it('correctly instantiates a simplex grid - only positive quotes', function () {
         var quotesList = [[1,2],[3,4]];
         var expectedPoints = [[0,0],[1,0],[3,0],[0,3],[1,3],[3,3],[0,7],[1,7],[3,7]];
-        var expectedComplex = [[0,1,3],[4,3,1],[1,2,4],[5,4,2],[3,4,6],[7,6,4],[4,5,7],[8,7,5]];
+        var expectedComplex = [[0,1,3],[1,4,3],[1,2,4],[2,5,4],[3,4,6],[4,7,6],[4,5,7],[5,8,7]];
         var simplexgrid = simplexn.geometries.simplexGrid(quotesList);
         var simpcomp = new simplexn.SimplicialComplex(expectedPoints, expectedComplex);
 
@@ -17,7 +17,7 @@
       it('correctly instantiates a simplex grid - positive and negative quotes', function () {
         var quotesList = [[1],[3,-1,4]];
         var expectedPoints = [[0,0],[1,0],[0,3],[1,3],[0,4],[1,4],[0,8],[1,8]];
-        var expectedComplex = [[0,1,2],[3,2,1],[4,5,6],[7,6,5]];
+        var expectedComplex = [[0,1,2],[1,3,2],[4,5,6],[5,7,6]];
         var simplexgrid = simplexn.geometries.simplexGrid(quotesList);
         var simpcomp = new simplexn.SimplicialComplex(expectedPoints, expectedComplex);
 
@@ -47,7 +47,7 @@
       it('correctly instantiates a simplex grid - two level of quotes, negative quotes', function () {
         var quotesList = [[1,-1,1],[2,-2,2]];
         var expectedPoints = [[0,0],[1,0],[2,0],[3,0],[0,2],[1,2],[2,2],[3,2],[0,4],[1,4],[2,4],[3,4],[0,6],[1,6],[2,6],[3,6]];
-        var expectedComplex = [[0,1,4],[5,4,1],[2,3,6],[7,6,3],[8,9,12],[13,12,9],[10,11,14],[15,14,11]];
+        var expectedComplex = [[0,1,4],[1,5,4],[2,3,6],[3,7,6],[8,9,12],[9,13,12],[10,11,14],[11,15,14]];
         var simplexgrid = simplexn.geometries.simplexGrid(quotesList);
         var simpcomp = new simplexn.SimplicialComplex(expectedPoints, expectedComplex);
 
@@ -67,7 +67,7 @@
       it('correctly instantiates a simplex grid - two level of quotes, starting with a negative quote', function () {
         var quotesList = [[-2,1,-1,2], [-3,1,-1,3]];
         var expectedPoints = [[2,3],[3,3],[4,3],[6,3],[2,4],[3,4],[4,4],[6,4],[2,5],[3,5],[4,5],[6,5],[2,8],[3,8],[4,8],[6,8]];
-        var expectedComplex = [[0,1,4],[5,4,1],[2,3,6],[7,6,3],[8,9,12],[13,12,9],[10,11,14],[15,14,11]];
+        var expectedComplex = [[0,1,4],[1,5,4],[2,3,6],[3,7,6],[8,9,12],[9,13,12],[10,11,14],[11,15,14]];
         var simplexgrid = simplexn.geometries.simplexGrid(quotesList);
         var simpcomp = new simplexn.SimplicialComplex(expectedPoints, expectedComplex);
 
@@ -79,7 +79,7 @@
       it('correctly instantiates a cuboidal simplicial complex', function () {
         var cuboid = simplexn.geometries.cuboid([3,2,1]);
         var points = [[0,0,0],[3,0,0],[0,2,0],[3,2,0],[0,0,1],[3,0,1],[0,2,1],[3,2,1]];
-        var cells = [[0,1,2,4],[1,2,4,5],[2,4,5,6],[3,2,1,7],[2,1,7,6],[1,7,6,5]];
+        var cells = [[0,1,2,4],[1,2,4,5],[2,4,5,6],[1,3,2,5],[3,2,5,7],[2,5,7,6]];
         var simpcomp = new simplexn.SimplicialComplex(points,cells);
 
         expect(cuboid.equals(simpcomp)).to.be.ok();
@@ -90,7 +90,7 @@
       it('correctly instantiates a 3D cube', function () {
         var cube = simplexn.geometries.cube(3);
         var points = [[0,0,0],[1,0,0],[0,1,0],[1,1,0],[0,0,1],[1,0,1],[0,1,1],[1,1,1]];
-        var cells = [[0,1,2,4],[1,2,4,5],[2,4,5,6],[3,2,1,7],[2,1,7,6],[1,7,6,5]];
+        var cells = [[0,1,2,4],[1,2,4,5],[2,4,5,6],[1,3,2,5],[3,2,5,7],[2,5,7,6]];
         var simpcomp = new simplexn.SimplicialComplex(points,cells);
 
         expect(cube.equals(simpcomp)).to.be.ok();
